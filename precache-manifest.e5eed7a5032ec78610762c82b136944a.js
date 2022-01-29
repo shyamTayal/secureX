013a4cf3407fa9bacb7d70e225b18d154a8f3c1e
Hashir <mail2hashirpm@gmail.com>
2022-01-29 22:29:40 +0530

    build

---
+++ precache-manifest.e5eed7a5032ec78610762c82b136944a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a37df2ccfe09363d6ca9",
+    "url": "/secureX/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "a37df2ccfe09363d6ca9",
+    "url": "/secureX/static/js/main.ba76ee09.chunk.js"
+  },
+  {
+    "revision": "d18b3d7343cd35e3ca4b",
+    "url": "/secureX/static/js/runtime~main.0724a787.js"
+  },
+  {
+    "revision": "fe4eb9ba1b0b260cf2c6",
+    "url": "/secureX/static/css/2.bbc8216b.chunk.css"
+  },
+  {
+    "revision": "fe4eb9ba1b0b260cf2c6",
+    "url": "/secureX/static/js/2.7d64ff6a.chunk.js"
+  },
+  {
+    "revision": "2d01ae7d8a4aad5f0fec92e9cf94aab4",
+    "url": "/secureX/index.html"
+  }
+];
